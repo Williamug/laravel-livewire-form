9323eec48eba280fd7b8e4a8057201993b6ee7ba
williamdk <asabawilliam@gmail.com>
2020-12-22 16:13:57 +0300

    updates

--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -1,28 +1,30 @@
 <div>
     <form wire:submit.prevent="submit">
-    	<div class="form-group">
-    		<label for="exampleInputName">Name</label>
-    		<input type="text" class="form-control" id="exampleInputName" placeholder="Enter name" wire:model="name">
-    		@error('name')
-    			<span class="text-danger">{{ $message}}</span>
-    		@enderror
-    	</div>
+        <div class="justify-center">
+        	<div class="">
+        		<label for="exampleInputName" class="block">Name</label>
+        		<input type="text" class="rounded" id="exampleInputName" placeholder="Enter name" wire:model="name">
+        		@error('name')
+        			<span class="text-danger">{{ $message}}</span>
+        		@enderror
+        	</div>
 
-    	<div class="form-group">
-    		<label for="exampleInputEmail">Email</label>
-    		<input type="text" class="form-control" id="exampleInputEmail" placeholder="Enter email" wire:model="email">
-    		@error('email')
-    			<span class="text-danger">{{ $message}}</span>
-    		@enderror
-    	</div>
+        	<div class="form-group">
+        		<label for="exampleInputEmail" class="block">Email</label>
+        		<input type="text" class="rounded" id="exampleInputEmail" placeholder="Enter email" wire:model="email">
+        		@error('email')
+        			<span class="text-danger">{{ $message}}</span>
+        		@enderror
+        	</div>
 
-    	<div class="form-group">
-    		<label for="exampleInputbody">Body</label>
-    		<textarea id="exampleInputbody"  class="form-control" placeholder="Enter Body" wire:model="body"></textarea>
-    		@error('body')
-    			<span class="text-danger">{{ $message }}</span>
-    		@enderror
-    	</div>
-    	<button type="submit">Save</button>
+        	<div class="form-group">
+        		<label for="exampleInputbody" class="block">Body</label>
+        		<textarea id="exampleInputbody"  class="rounded" placeholder="Enter Body" wire:model="body"></textarea>
+        		@error('body')
+        			<span class="text-danger">{{ $message }}</span>
+        		@enderror
+        	</div>
+        	<button type="submit">Save</button>
+        </div>
     </form>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,12 @@ function () {
 		return view('welcome');
 	});
 
+Route::get('/dashboard', function () {
+		return view('dashboard');
+	})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ .'/auth.php';
+
 Route::get('/form', function () {
 		return view('form');
 	});
